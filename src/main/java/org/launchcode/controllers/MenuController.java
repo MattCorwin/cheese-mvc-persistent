5bc6e66201591d7bb09800ef4ca4945cf6caa129
Matt Corwin <corwin.matt@gmail.com>
2019-01-24 15:15:53 -0600

    Added category controller and paths, and started to build menu functionality

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,44 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+
+    // Request path: /menu
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "My Menus");
+
+        return "menu/index";
+    }
+
+    //Request path: /menu/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+
+
+    }
+
+}
